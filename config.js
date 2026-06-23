310b9188fbb0ae9474e3f8642235cef1c40f4080
xoo59568-art <xoo59568@gmail.com>
2026-06-23 14:07:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ module.exports = {
 	READ_MESSAGES: toBool(process.env.READ_MESSAGES || "false"),
 	REJECT_CALL: toBool(process.env.REJECT_CALL || "false"),
 	REJECT_CALL_MSG: process.env.REJECT_CALL_MSG || "_Calls are not allowed. Please don’t call again!._",
-	SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:204742989fe2970f49c4cb04f4345e9c",
+	SESSION_ID: process.env.SESSION_ID || "RABBITXMD-XCIChCzS#TXTascCwzqBg-s5CMaR5aUl4OIEhXQa8XS9CTWWPtc4",
 	START_MSG: toBool(process.env.START_MSG || "true"),
 	STICKER_DATA: process.env.STICKER_DATA || `X BOT MD;ASWIN SPARKY`,
 	SUDO: process.env.SUDO || "917012984396",
